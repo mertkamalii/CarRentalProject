d040e63153d90a84c3ae8c3cc5c675e920656a30
Mert KAMALI <74986145+mertkamalii@users.noreply.github.com>
2022-12-22 13:34:41 +0300

    DbContext Ve Entity Package Dahil Etme

--- CarRentalProject/Models/Siniflar/AdresBlog.cs
+++ CarRentalProject/Models/Siniflar/AdresBlog.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace CarRentalProject.Models.Siniflar
 {
-    public class Adres
+    public class AdresBlog
     {
         [Key]
         public int ID { get; set; }
---
+++ CarRentalProject/Models/Siniflar/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CarRentalProject.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<AdresBlog> AdresBlogs { get; set; }
+        public DbSet<Subeler> Subelers { get; set; }
+        public DbSet<Hakkimizda> Hakkiimizdas { get; set; }
+        public DbSet<Iletisim> Iletisims { get; set; }        
+        public DbSet<Yorumlar> Yorumlars { get; set; }
+    }
+}
